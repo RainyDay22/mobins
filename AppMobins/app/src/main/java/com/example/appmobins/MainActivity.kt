26e75145ae064241a0b659e0b53d556026157260
Ruining D <ruiningd@Ruinings-MacBook-Air.local>
2024-08-12 17:14:29 +0200

    Added toggle switch to second page, made activity inheritance more advanced, changed theme

--- AppMobins/app/src/main/java/com/example/appmobins/MainActivity.kt
+++ AppMobins/app/src/main/java/com/example/appmobins/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,7 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appmobins.ui.theme.AppMobinsTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
--- AppMobins/app/src/main/java/com/example/appmobins/PageActivity.kt
+++ AppMobins/app/src/main/java/com/example/appmobins/PageActivity.kt
@@ -5,8 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class PageActivity : ComponentActivity() {
+class PageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_layout)
@@ -16,5 +17,18 @@ class PageActivity : ComponentActivity() {
                 Log.d("NAV", "Ended new activity")
                 finish()
             }
+
+
+        findViewById<com.google.android.material.switchmaterial.SwitchMaterial>(R.id.material_switch)
+            .setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // The switch is checked.
+                Log.d("Switch", "switch toggled on")
+            } else {
+                // The switch isn't checked.
+                Log.d("Switch", "switch toggled off")
+            }
+            }
+
     }
 }
